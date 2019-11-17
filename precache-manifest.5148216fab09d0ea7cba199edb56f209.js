897557a7847e0d8d11337cecc684d8188be8b5de
JGardener <jamesgardener92@gmail.com>
2019-11-17 19:09:38 +0000

    Updates

---
+++ precache-manifest.5148216fab09d0ea7cba199edb56f209.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c2e38236eca556b1e860c93373f0e5",
+    "url": "/rock-paper-scissors-react/index.html"
+  },
+  {
+    "revision": "071a199085eaef7cceca",
+    "url": "/rock-paper-scissors-react/static/css/main.bebe5f82.chunk.css"
+  },
+  {
+    "revision": "4ae167a39918c5f6d64e",
+    "url": "/rock-paper-scissors-react/static/js/2.0f6b344d.chunk.js"
+  },
+  {
+    "revision": "071a199085eaef7cceca",
+    "url": "/rock-paper-scissors-react/static/js/main.3387acaf.chunk.js"
+  },
+  {
+    "revision": "abd177be2db81b6cf55f",
+    "url": "/rock-paper-scissors-react/static/js/runtime~main.40c7457e.js"
+  }
+]);
